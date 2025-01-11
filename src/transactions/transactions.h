db69d5f30a43808455ef8a8735f5fd33a2d53905
hornet468 <tomhelington@gmail.com>
2025-01-11 16:48:18 +0000

    update transaction

--- src/transactions/transactions.h
+++ src/transactions/transactions.h
@@ -7,13 +7,16 @@ class Transactions {
 private:
     std::string sender;
     std::string receiver;
+    std::string signature;
     double amount;
 
 public:
-    Transactions(const std::string& sn, const std::string& rv, const double am);
+    Transactions(const std::string& sn, const std::string& rv, const double am, const std::string& sig);
 
     std::string getSender() const;
     std::string getReceiver() const;
+    std::string setSignature(const std::string& sig);
+    std::string getSignature() const;
     double getAmount() const;
 };
 
